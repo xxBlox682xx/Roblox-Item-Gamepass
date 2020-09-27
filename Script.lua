0f0de62bc18f766becb3480f5adc4a9ce5e2e151
xxBlox682xx <71976107+xxBlox682xx@users.noreply.github.com>
2020-09-27 18:18:07 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -10,3 +10,22 @@ game.Players.PlayerAdded:Connect(function(player)
  end
 end)
 
+
+-- BUTTON VERSION(MAY NOT BE WORKING I THINK):
+
+
+
+local MarketPlaceService = game:GetService("MarketplaceService")
+local GamepassID = ID HERE
+
+script.Parent.Mousebutton1click:Connect(function()
+   
+game.Players.PlayerAdded:Connect(function(player)
+ 
+ if MarketPlaceService:UserOwnsGamePassAsync(player.UserId, GamepassID) then
+  
+  game.ServerStorage.GamePassTool:Clone().Parent = player:WaitForChild("Backpack")
+  game.ServerStorage.GamePassTool:Clone().Parent = player:WaitForChild("StarterGear")
+ end
+end)
+ 
